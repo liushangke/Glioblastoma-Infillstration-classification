468c33cc21db4aba536a38617ce219ed63b51724
liushangke <shangke_liu@outlook.com>
2023-11-28 13:07:29 -0600

    init

--- dataloader.py
+++ dataloader.py
@@ -12,6 +12,8 @@ class PatientDataset(Dataset):
         self.patch_size = (16, 16, 16)  # Desired patch size
         self.patches_per_sample = patches_per_sample  # Number of patches to extract per sample
         self.resize_shape = resize_shape
+        self.patient_ids = [patient['patient_id'] for patient in all_patient_data]
+
 
     def map_label(self, label):
         if label == "Extensive":
@@ -20,7 +22,21 @@ class PatientDataset(Dataset):
             return [0, 1, 0]
         else:
             return [0, 0, 1]
+
+    # def map_label(self, label):
+    #     return 1 if label == "Extensive" else 0
+
         
+    def inverse_map_label(self, one_hot_label):
+        label_map = {
+            "Extensive": [1, 0, 0],
+            "None": [0, 1, 0],
+            "Microscopic": [0, 0, 1]
+        }
+        for label, one_hot in label_map.items():
+            if one_hot == one_hot_label:
+                return label
+   
 
     def resize_sequence(self, sequence):
         # Use scipy's zoom to resize the sequence based on the resize_shape attribute
@@ -35,10 +51,11 @@ class PatientDataset(Dataset):
 
     def __getitem__(self, index):
         # Determine the actual patient sample and patch index
-        patient_idx = index // self.patches_per_sample
+        patient_index = index // self.patches_per_sample
+        patient_id = self.patient_ids[patient_index]
         patch_idx = index % self.patches_per_sample  # This is not used but is here for clarity
         
-        patient_data = self.data[patient_idx]
+        patient_data = self.data[patient_index]
 
         # Process the sequences
         sequence_types = ['T1-3D.nii', 'T1c-3D.nii', 'T2-3D.nii', 'FLAIR-3D.nii']
@@ -61,9 +78,120 @@ class PatientDataset(Dataset):
 
         # Stack the sequences into a single tensor
         tensor = torch.stack(sequences).squeeze(1)
-        label = torch.tensor(self.labels[patient_idx], dtype=torch.float32)
+        label = torch.tensor(self.labels[patient_index], dtype=torch.float32)
 
-        return tensor, label, patient_idx
+        return tensor, label, patient_id
+
+# class PatientDatasetBinary(Dataset):
+#     def __init__(self, all_patient_data, labels_array, patches_per_sample=10, resize_shape=(100, 100, 100)):
+#         self.data = all_patient_data
+#         # Replicate each label 'patches_per_sample' times to match the number of patches
+#         self.labels = [self.map_label(labels_array[patient_idx]) for patient_idx, _ in enumerate(all_patient_data) for _ in range(patches_per_sample)]
+#         self.patch_size = (16, 16, 16)  # Desired patch size
+#         self.patches_per_sample = patches_per_sample  # Number of patches to extract per sample
+#         self.resize_shape = resize_shape
+#         self.patient_ids = [patient['patient_id'] for patient in all_patient_data]
+
+#     def map_label(self, label):
+#         return 1 if label == "Extensive" else 0
+
+#     def resize_sequence(self, sequence):
+#         # Use scipy's zoom to resize the sequence based on the resize_shape attribute
+#         factors = (self.resize_shape[0]/sequence.shape[0], 
+#                    self.resize_shape[1]/sequence.shape[1], 
+#                    self.resize_shape[2]/sequence.shape[2])
+#         return resize(sequence, factors)
+                                 
+#     def __len__(self):
+#         return len(self.data) * self.patches_per_sample
+
+#     def __getitem__(self, index):
+#         # Determine the actual patient sample and patch index
+#         patient_index = index // self.patches_per_sample
+#         patient_id = self.patient_ids[patient_index]
+#         patch_idx = index % self.patches_per_sample  # This is not used but is here for clarity
+        
+#         patient_data = self.data[patient_index]
+
+#         # Process the sequences
+#         sequence_types = ['T1-3D.nii', 'T1c-3D.nii', 'T2-3D.nii', 'FLAIR-3D.nii']
+#         sequences = []
+#         for sequence_type in sequence_types:
+#             tensor = patient_data[sequence_type].astype(np.float32)
+#             tensor = self.resize_sequence(tensor)
+            
+#             # Randomly select a starting point for the patch
+#             start_x = np.random.randint(0, tensor.shape[0] - self.patch_size[0])
+#             start_y = np.random.randint(0, tensor.shape[1] - self.patch_size[1])
+#             start_z = np.random.randint(0, tensor.shape[2] - self.patch_size[2])
+
+#             # Extract the patch
+#             patch = tensor[start_x:start_x + self.patch_size[0],
+#                            start_y:start_y + self.patch_size[1],
+#                            start_z:start_z + self.patch_size[2]]
+
+#             sequences.append(torch.from_numpy(patch))
+
+#         # Stack the sequences into a single tensor
+#         tensor = torch.stack(sequences).squeeze(1)
+#         label = torch.tensor(self.labels[patient_index], dtype=torch.long)  # Ensure the label is of type long
+
+#         return tensor, label, patient_id
+
+class PatientDatasetBinary(Dataset):
+    def __init__(self, all_patient_data, labels_array, resize_shape=(100, 100, 100), augment=True):
+        self.data = all_patient_data
+        self.labels = [self.map_label(label) for label in labels_array]
+        self.resize_shape = resize_shape
+        self.patient_ids = [patient['patient_id'] for patient in all_patient_data]
+        self.augment = augment
+
+        # Define augmentation transforms using torchio
+        self.transforms = tio.Compose([
+            tio.RandomFlip(axes=(0, 1, 2)),
+            tio.RandomAffine(degrees=(10, 10, 10), scales=(0.9, 1.1)),
+            tio.RandomNoise(std=0.5),
+            tio.RandomElasticDeformation(),
+        ])
+
+    def map_label(self, label):
+        return 1 if label == "Extensive" else 0
+
+    def resize_sequence(self, sequence):
+        # Use scipy's zoom to resize the sequence based on the resize_shape attribute
+        factors = (self.resize_shape[0] / sequence.shape[0], 
+                   self.resize_shape[1] / sequence.shape[1], 
+                   self.resize_shape[2] / sequence.shape[2])
+        return resize(sequence, factors)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        patient_data = self.data[index]
+        patient_id = self.patient_ids[index]
+
+        # Process the sequences
+        sequence_types = ['T1-3D.nii', 'T1c-3D.nii', 'T2-3D.nii', 'FLAIR-3D.nii']
+        sequences = []
+        for sequence_type in sequence_types:
+            tensor = patient_data[sequence_type].astype(np.float32)
+            tensor = self.resize_sequence(tensor)
+            
+            # Apply augmentation
+            if self.augment:
+                subject = tio.Subject(mri=tio.ScalarImage(tensor=tensor))
+                transformed_subject = self.transforms(subject)
+                tensor = transformed_subject['mri']['data'].numpy()
+
+            sequences.append(torch.from_numpy(tensor))
+
+        # Stack the sequences into a single tensor
+        tensor = torch.stack(sequences)
+        label = torch.tensor(self.labels[index], dtype=torch.long)  # Ensure the label is of type long
+
+        return tensor, label, patient_id
+
 
 def display_dataset_info(dataset):
     # Total Length of the Dataset
--- train.py
+++ train.py
@@ -1,38 +1,29 @@
 import os
-import glob
-import nrrd
-import nibabel as nib
 import numpy as np
-import re
 import matplotlib.pyplot as plt
-import pandas as pd
 import seaborn as sns
-from collections import defaultdict
-from mpl_toolkits.mplot3d import Axes3D
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
-from scipy.ndimage import binary_opening
-from scipy.ndimage import zoom as resize
+import logging
 
 from dataloader import PatientDataset
 
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader
 import monai
 from monai.networks.nets import resnet10
 
 from sklearn.model_selection import StratifiedKFold
-from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.metrics import confusion_matrix
+
 
 import torch.optim as optim
 import torch.nn as nn
-
-import os
 import datetime
-import logging
 
-
-data = np.load('/home/slt2870/Glioblastoma_Infillstration_Classification/processed_patient_data.npy', allow_pickle=True)
-labels = np.load('/home/slt2870/Glioblastoma_Infillstration_Classification/labels.npy', allow_pickle=True)
+# Load data
+data_path = '/home/slt2870/Glioblastoma_Infillstration_Classification/processed_patient_data.npy'
+labels_path = '/home/slt2870/Glioblastoma_Infillstration_Classification/labels.npy'
+data = np.load(data_path, allow_pickle=True)
+labels = np.load(labels_path, allow_pickle=True)
 
 
 # Parameters
@@ -43,98 +34,192 @@ n_input_channels = 4
 num_classes = 3
 num_epochs = 10
 
-# Define log directory and filename
+
+def plot_curves(y_values1, y_values2, title, y_label, fold, curve_type):
+    epochs = range(1, len(y_values1) + 1)
+    plt.figure(figsize=(10, 6))
+    plt.plot(epochs, y_values1, 'b', label='Training')
+    plt.plot(epochs, y_values2, 'r', label='Validation')
+    plt.title(title)
+    plt.xlabel('Epochs')
+    plt.ylabel(y_label)
+    plt.legend()
+    plt.grid(True)
+    plt.savefig(os.path.join(log_dir, f"{curve_type}_curves_fold_{fold}_{model_name}_{current_date}.png"))
+    plt.close()
+
+def plot_confusion_matrix(y_true, y_pred, classes, fold):
+
+    # Get unique labels from y_true
+    unique_labels = np.unique(y_true)
+    matrix = confusion_matrix(y_true, y_pred, labels=unique_labels)
+    
+    plt.figure(figsize=(8, 6))
+    sns.heatmap(matrix, annot=True, fmt='d', cmap='Blues', 
+                xticklabels=unique_labels, yticklabels=unique_labels)
+    plt.xlabel('Predicted Label')
+    plt.ylabel('True Label')
+    plt.title(f'Confusion Matrix for Fold {fold}')
+    plt.savefig(os.path.join(log_dir, f"confusion_matrix_fold_{fold}_{model_name}_{current_date}.png"))
+    plt.close()
+
+
+
+# Logging setup
 log_dir = "/home/slt2870/Glioblastoma_Infillstration_Classification/logs"
 log_file = f"{model_name}_{current_date}_lr{lr}_epochs{num_epochs}.txt"
 log_path = os.path.join(log_dir, log_file)
 
 save_model_dir = "/home/slt2870/Glioblastoma_Infillstration_Classification/model_weights"
+
 if not os.path.exists(save_model_dir):
     os.makedirs(save_model_dir)
 
-
-# Check and create log directory if it doesn't exist
 if not os.path.exists(log_dir):
     os.makedirs(log_dir)
 
-# Set up logging
 logging.basicConfig(filename=log_path, level=logging.INFO, 
                     format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 logger = logging.getLogger()
-logger.addHandler(logging.StreamHandler())  # Also log to console
+logger.addHandler(logging.StreamHandler())
 
-# Set device
+
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# Set up 4-fold stratified cross-validation
-kf = StratifiedKFold(n_splits=4, shuffle=True, random_state=42)
-for fold, (train_idx, test_idx) in enumerate(kf.split(data, labels)):
-    
-    train_data, train_labels = data[train_idx], labels[train_idx]
-    test_data, test_labels = data[test_idx], labels[test_idx]
-    
-    train_dataset = PatientDataset(train_data, train_labels, patches_per_sample=10)
-    test_dataset = PatientDataset(test_data, test_labels, patches_per_sample=10)
-    
-    train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=2, shuffle=False)
-    
-    # Initialize the ResNet model
-    model = resnet10(spatial_dims=3, n_input_channels=n_input_channels, num_classes=num_classes, pretrained=False)
-    model.to(device)  # Transfer model to GPU
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+# Extract indices for the 'None' samples
+nan_indices = [i for i, label in enumerate(labels) if label == 'nan']
+print(len(nan_indices))
+# Exclude 'nan' samples from the original data for the stratified split
+data_no_nan = np.delete(data, nan_indices, axis=0)
+labels_no_nan = np.delete(labels, nan_indices, axis=0)
+
+kf = StratifiedKFold(n_splits=4, shuffle=True, random_state=42)
+
+for fold, (train_idx, val_idx) in enumerate(kf.split(data_no_nan, labels_no_nan)):
+    train_losses = []
+    val_losses = []
+    train_accuracies = []
+    val_accuracies = []
+    misclassified_patient_ids = []
+
+    # Create a mapping of indices from data_no_nan to original data
+    mapping = [i for i in range(len(data)) if i not in nan_indices]
+
+    # Map the train and validation indices from data_no_nan to original data
+    train_idx = [mapping[i] for i in train_idx]
+    val_idx = [mapping[i] for i in val_idx]
+
+    # Adjust indices to include 'nan' samples
+    if fold % 2 == 0:  # For even folds
+        train_idx = np.concatenate([train_idx, [nan_indices[0]]])
+        val_idx = np.concatenate([val_idx, [nan_indices[1]]])
+    else:  # For odd folds
+        train_idx = np.concatenate([train_idx, [nan_indices[1]]])
+        val_idx = np.concatenate([val_idx, [nan_indices[0]]])
+
+
+    print("Train indices for Fold ", fold, " : ", train_idx)
+    print("Validation indices for Fold ", fold, " : ", val_idx)
+
+
+    # Reset the train and validation data/labels using the new indices
+    train_data, train_labels = data[train_idx], labels[train_idx]
+    val_data, val_labels = data[val_idx], labels[val_idx]
     
-    # Training loop
+    print("Train labels for Fold ", fold, " : ", train_labels)
+    print("Validation labels for Fold ", fold, " : ", val_labels)
+
+
+    # Count occurrences of each label in the training and validation sets
+    unique_train_labels, train_counts = np.unique(train_labels, return_counts=True)
+    unique_val_labels, val_counts = np.unique(val_labels, return_counts=True)
+
+    # Log or print the results
+    logger.info(f"Fold {fold + 1} Training Data:")
+    for label, count in zip(unique_train_labels, train_counts):
+        logger.info(f"Class {label}: {count} samples")
+
+    logger.info(f"Fold {fold + 1} Validation Data:")
+    for label, count in zip(unique_val_labels, val_counts):
+        logger.info(f"Class {label}: {count} samples")
+
+    train_dataset = PatientDataset(train_data, train_labels, patches_per_sample=10)
+    val_dataset = PatientDataset(val_data, val_labels, patches_per_sample=10)
+
+    train_loader = DataLoader(train_dataset, batch_size=2, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=2, shuffle=False)
+
+    model = resnet10(spatial_dims=3, n_input_channels=n_input_channels, num_classes=num_classes, pretrained=False)
+    model.to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+    
     for epoch in range(num_epochs):
+        # Training
         model.train()
-        epoch_loss = 0
-        for inputs, targets, patient_idxs in train_loader:  # Updated this line
+        epoch_train_loss = 0
+        correct_train = 0
+        total_train = 0
+
+        for inputs, targets, _ in train_loader:
             inputs, targets = inputs.to(device), targets.to(device)
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, torch.argmax(targets, dim=1))
             loss.backward()
             optimizer.step()
-            epoch_loss += loss.item()
 
-
-        print(f"Fold {fold + 1}, Epoch {epoch + 1}, Training Loss: {epoch_loss/len(train_loader):.4f}")
-        
-    # Test loop
-    model.eval()
-    total_correct = 0
-    total_samples = 0
-    all_preds = []
-    all_labels = []
-    misclassified_patient_ids = []
-    
-    with torch.no_grad():
-        for inputs, targets, patient_idxs in test_loader:  # Assume dataset returns patient_idxs
-            inputs, targets = inputs.to(device), targets.to(device)  # Transfer data to GPU
-            outputs = model(inputs)
+            epoch_train_loss += loss.item()
             _, predicted = outputs.max(1)
-            total_samples += targets.size(0)
-            total_correct += (predicted == torch.argmax(targets, dim=1)).sum().item()
-            all_preds.extend(predicted.cpu().numpy())
-            all_labels.extend(torch.argmax(targets, dim=1).cpu().numpy())
-            
-            # Track misclassified samples
-            for i, pred in enumerate(predicted):
-                if pred != torch.argmax(targets[i], dim=0):
-                    misclassified_patient_ids.append(patient_idxs[i].item())
-    
-    accuracy = total_correct / total_samples
-    precision = precision_score(all_labels, all_preds, average='macro')
-    recall = recall_score(all_labels, all_preds, average='macro')
-    f1 = f1_score(all_labels, all_preds, average='macro')
-    
-    print(f"Fold {fold + 1}, Test Accuracy: {accuracy:.2f}, Precision: {precision:.2f}, Recall: {recall:.2f}, F1 Score: {f1:.2f}")
-    print(f"Misclassified Patient IDs in Fold {fold + 1}: {misclassified_patient_ids}")
+            total_train += targets.size(0)
+            correct_train += (predicted == torch.argmax(targets, dim=1)).sum().item()
 
-    # Save model weights
+        train_losses.append(epoch_train_loss / len(train_loader))
+        train_accuracies.append(100 * correct_train / total_train)
+
+        # Validation
+        model.eval()
+        epoch_val_loss = 0
+        correct_val = 0
+        total_val = 0
+        all_labels = []
+        all_preds = []
+
+        for inputs, targets, patient_idxs in val_loader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, torch.argmax(targets, dim=1))
+            epoch_val_loss += loss.item()
+
+            _, predicted = outputs.max(1)
+            total_val += targets.size(0)
+            correct_val += (predicted == torch.argmax(targets, dim=1)).sum().item()
+            all_labels.extend(torch.argmax(targets, dim=1).cpu().numpy())
+            all_preds.extend(predicted.cpu().numpy())
+
+            if epoch == num_epochs - 1:
+                for i, pred in enumerate(predicted):
+                    if pred != torch.argmax(targets[i], dim=0):
+                        misclassified_patient_ids.append(patient_idxs[i].item())
+
+        val_losses.append(epoch_val_loss / len(val_loader))
+        val_accuracies.append(100 * correct_val / total_val)
+
+        logger.info(f"Fold {fold + 1}, Epoch {epoch + 1}, Training Loss: {train_losses[-1]:.4f}, Validation Loss: {val_losses[-1]:.4f}")
+        logger.info(f"Training Accuracy: {train_accuracies[-1]:.2f}, Validation Accuracy: {val_accuracies[-1]:.2f}")
+
+        if epoch == num_epochs - 1:
+            logger.info(f"Misclassified Patient IDs for Fold {fold + 1}: {misclassified_patient_ids}")
+
+    plot_curves(train_losses, val_losses, f'Training and Validation Loss for Fold {fold + 1}', 'Loss', fold + 1, 'loss')
+    plot_curves(train_accuracies, val_accuracies, f'Training and Validation Accuracy for Fold {fold + 1}', 'Accuracy (%)', fold + 1, 'accuracy')
+    unique_labels = np.unique(all_labels)
+    plot_confusion_matrix(all_labels, all_preds, unique_labels, fold + 1)
+
     model_save_path = os.path.join(save_model_dir, f"model_fold_{fold + 1}.pth")
     torch.save(model.state_dict(), model_save_path)
 
-print('Finished Training')
+
+
+logger.info('Finished Training')
